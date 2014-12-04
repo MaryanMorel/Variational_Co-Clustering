9053bd9ec07c8762c5e98e378116d6cac571f3bf
MoussabDjerrab <root@ns3366590.ip-37-187-78.eu>
2014-12-04 19:46:38 +0100

    new version of initialisation

---
+++ code_inspect_data.py
@@ -0,0 +1,11 @@
+/*Les fichiers data sont dans ml-100k*/
+import os
+import pandas as pd
+
+/* on change de directory pour travailler */
+os.getcwd()
+os.chdir("/root/Bureau/PGM_PROJECT/ml-100k")
+
+
+df1 = pd.read
+
