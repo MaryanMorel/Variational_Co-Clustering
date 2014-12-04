35feeff53e607b99ca874e765b85c55a63127e15
MoussabDjerrab <root@ns3366590.ip-37-187-78.eu>
2014-12-04 20:47:40 +0100

    ajout des initialisations des probabilités de cluster

--- class_coclustering.py
+++ class_coclustering.py
@@ -16,7 +16,7 @@ data1 = berno.rvs((20,10))
 
 	def __init__(self, phi, data, mu, nx, ny, 
 			n_initx,n_clusterx, n_inity,n_clustery,
-			q_x, q_y, p_phi, phi,
+			q_x, self.q_y, p_phi, phi,
 			EM_n_cluster):
 		self.EM_n_cluster = EM_n_cluster
 		self.data =  # we need to generate some 
@@ -46,27 +46,27 @@ data1 = berno.rvs((20,10))
 		indic_y = self.kmeany.predict(self.data)
 
 			## intialization of q 
-		q_x = np.zeros((self.data_row, n_clusterx))		
-		q_y = np.zeros((self.data_col, n_clustery))
+		self.q_x = np.zeros((self.data_row, n_clusterx))		
+		self.q_y = np.zeros((self.data_col, n_clustery))
 		for i in [1:self.data_row]:
 			for j in [1:self.n_clusterx]:
 				if ( indic_x(i) == j):
-					q_x(i,j) = 1
+					self.q_x(i,j) = 1
 				else:
-					q_x(i,j) = 0
+					self.q_x(i,j) = 0
 		for i in [1:self.data_col]:
 			for j in [1:self.n_clustery]:
 				if ( indic_y(i) == j):
-					q_y(i,j) = 1
+					self.q_y(i,j) = 1
 				else:
-					q_y(i,j) = 0
+					self.q_y(i,j) = 0
 			## filling p_phi
 		self.phi = np.zeros((self.data_row,self.data_col,self.n_clusterx,self.n_clustery))
 		for i in [1:self.data_row]:
 			for j in [1:self.data_col]:
 				for i1 in [1:self.n_clusterx]:
 					for j1 in [1:self.n_clustery]:
-						self.p_phi(i,j,i1,j1) = q_x(i,i1)*q_y(j,j1)
+						self.p_phi(i,j,i1,j1) = self.q_x(i,i1)*self.q_y(j,j1)
 		###########################
 
 		## intialization of phi  ###########################
@@ -91,7 +91,31 @@ data1 = berno.rvs((20,10))
 
 
 	def EM_step(self,EM_n_cluster):
+	## for i in [1:EM_n_cluster]
 
 	## inner E-step:
-		self.
+		## self.q_x part
+		for i in [1:self.data_row]:
+			for i1 in [1:self.n_clusterx]:
+				self.q_x(i,i1) = 
+
+	## inner M-step:
+		for i1 in [1:self.n_clusterx]:
+			for j1 in [1:self.n_clustery]:
+				Numerator = 0
+				for i in [1:self.data_row]:
+					for j in [1:self.data_col]:
+						Numerator = Numerator + \
+							    self.p_phi(i,j,i1,j1)*self.data(i,j)
+				Denominator1 = 0	
+				for i in [1:self.data_row]:
+				### TODO : vérifier que l' on a bien une proba borné
+					Denominator1 = Denominator1 + np.sum(self.p_phi(i,:,i1,:), \
+										axis=[1,3])*self.nx(i)
+				Denominator2 = 0	
+				for j in [1:self.data_col]:
+				### TODO : vérifier que l' on a bien une proba borné
+					Denominator2 = Denominator2 + np.sum(self.p_phi(:,j,:,j1), \
+										axis=[0,2])*self.ny(j)
+
 	
